3ed1fe88c93e860c01db91798f15bf62f55d0af5
elshad685 <Tomik2018>
2019-06-27 11:56:59 +0400

    Write star

--- src/Star.java
+++ src/Star.java
@@ -18,17 +18,30 @@
       */
      public static void main(String[] args) {
          // TODO code application logic here
-         System.out.println("Ədədi daxil edin :");
-         Scanner scan=new Scanner(System.in);
-         int a=scan.nextInt();
-         for (int i = 1; i <=a; i++) {
+         Ters(7);
+         Duz(7);
+     }
+
+     public static void Duz(int a) {
+         for (int i = 1; i <= a; i++) {
              for (int j = 0; j < i; j++) {
                  System.out.print("*");
-
              }
              System.out.println("");
          }
 
+     }
+
+     public static void Ters(int a) {
+         for (int i = a; i > 0; i--) {
+             for (int j = 0; j <= i - 1; j++) {
+                 System.out.print(" ");
+             }
+             for (int k = 0; k < a + 1 - i; k++) {
+                 System.out.print("*");
+             }
+             System.out.println();
+         }
 
      }
 
